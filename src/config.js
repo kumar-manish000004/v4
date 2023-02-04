864727550bb5fb4a646b668909db4d9da421a959
kumar-manish000004 <124406146+kumar-manish000004@users.noreply.github.com>
2023-02-04 16:55:57 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,26 +1,38 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
-
+  siteTitle: 'Maneesh Kumar Sinha | Software Developer ',
+  siteDescription:
+    'Maneesh Sinha is a Upcoming Software Developer, who loves learning new things and helping tech beginners.',
+  siteKeywords:
+    'Maneesh Kumar Sinha, Maneesh Kumar, kumar_manish04, kumar_manish,kumar_manish04, software developer, software engineer, face mask, face mask detection, jamshedpur, LPU, Lovely Proffesional University',
+  siteUrl: 'https://github.com/KUMAR-MANISH04',
+  siteLanguage: 'en_US',
+  googleAnalyticsID: 'UA-45666519-2',
+  googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
+  name: 'Maneesh Kumar Sinha',
+  location: 'Jamshedpur, India',
+  email: '7050maneesh@gmail.com',
+  github: 'https://github.com/KUMAR-MANISH04',
+  twitterHandle: 'MANEESH84765536',
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
-    },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://github.com/KUMAR-MANISH04',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/mk-sinha/',
     },
     {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      name: 'Leetcode',
+      url: 'https://leetcode.com/kumar_manish04/',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/kumar_manish04',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/MANEESH84765536',
     },
   ],
 
@@ -30,11 +42,11 @@ module.exports = {
       url: '/#about',
     },
     {
-      name: 'Experience',
+      name: 'Education',
       url: '/#jobs',
     },
     {
-      name: 'Work',
+      name: 'Projects',
       url: '/#projects',
     },
     {
@@ -43,13 +55,15 @@ module.exports = {
     },
   ],
 
+  navHeight: 100,
+
   colors: {
     green: '#64ffda',
     navy: '#0a192f',
     darkNavy: '#020c1b',
   },
 
-  srConfig: (delay = 200, viewFactor = 0.25) => ({
+  srConfig: (delay = 200) => ({
     origin: 'bottom',
     distance: '20px',
     duration: 500,
@@ -61,7 +75,7 @@ module.exports = {
     mobile: true,
     reset: false,
     useDelay: 'always',
-    viewFactor,
+    viewFactor: 0.25,
     viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
   }),
 };
