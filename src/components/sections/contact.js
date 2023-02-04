ac2abeb15d87afe4d07d68f841d99953031f32a1
MANEESH SINHA <56319939+KUMAR-MANISH04@users.noreply.github.com>
2023-02-05 03:52:19 +0530

    Update contact.js

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,7 +60,7 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m currently looking for any new opportunities, my inbox is always open.
+        Although I’m currently for new opportunities, my inbox is always open.
         Whether you have a question or just want to say hi, I’ll get back to you!
       </p>
 
